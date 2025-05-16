6ba91525d6935b86275fa133e2d7ce46653b4ae4
Karan <k79639@gmail.com>
2025-05-16 13:33:55 -0400

    Added quotes and emojis data.

---
+++ script.js
@@ -0,0 +1,22 @@
+const quotes = [
+    "Idk why we picked this textbook",
+    "Go fuck off, you're nowhere near as significant as me",
+    "If there are any terrorists in this class, 151 Front Street W in Toronto is a great place to blow up",
+    "I wouldn't show you porn either",
+    "Don't go around telling your parents you have a physical connection with your teacher",
+    "Inspired me to kidnap somebody and write a demand letter",
+    "You dumbasses always think more is better",
+    "WiFi is a big swimming pool and there are still people peeing in it",
+    "I think I'm getting some kind of senile disease",
+    "During class you can walk out any time. Hopefully in a dramatic fashion",
+    "Let's say I go crazy, that actually happened a couple years ago",
+    "We got a bunch of idiots, their full title is programmer",
+    "This network should have been taken out and shot",
+    "WiFi is completely useless",
+    "I hate you all (Oops, did I say that?)",
+    "Why the hell do we use this useless piece of shit",
+    "Anyone still listen to radio? ...You do! Ha! Loser.",
+    "Okay, I'm officially fucking old now"
+];
+
+const emojis = ["💀", "👨‍🏫", "😈", "👻", "🧛", "👽"];
